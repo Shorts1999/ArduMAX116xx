d84897ec6f471a82a727ec696f7b6789a473d0e3
Sjors <sjorssmit1999@gmail.com>
2023-01-11 10:18:22 +0100

    update config and setup writes

--- src/MAX116xx.cpp
+++ src/MAX116xx.cpp
@@ -35,11 +35,12 @@ uint8_t MAX116xx::readRegister(uint8_t reg, uint8_t *data, uint8_t len) {
 }
 
 uint16_t MAX116xx::readADC(uint8_t channel) {
+  //TODO: insert channel selection code
   Wire.requestFrom(mAddress, 2);
 
   // Read the conversion result
   uint8_t data[2];
-  uint8_t err = Wire.readBytes(data, sizeof(data)/sizeof(data[0]));
+  uint8_t err = Wire.readBytes(data, sizeof(data) / sizeof(data[0]));
   if (err != 0) {
     uint16_t result = data[0];
     return ((result << 8) | data[1]) & 0x03ff;
@@ -48,26 +49,57 @@ uint16_t MAX116xx::readADC(uint8_t channel) {
   return 0;
 }
 
-uint8_t MAX116xx::readStatus(){
-  return 1;
+uint8_t MAX116xx::writeSetup(uint8_t setup) {
+  Wire.beginTransmission(mAddress);
+
+  Wire.write(setup | 0x80); //For setup, first bit is always 1
+
+  return Wire.endTransmission();
 }
 
 uint8_t MAX116xx::writeConfig(uint8_t config) {
   Wire.beginTransmission(mAddress);
 
-  Wire.write(config);
+  Wire.write(config & 0x7f); //For config, first bit is always 0
 
   return Wire.endTransmission();
 }
 
-uint8_t MAX116xx::readConfig() {
-  uint8_t data;
-  uint8_t err = readRegister(MAX116xx_CONFIG, &data);
-  if(err==0) return data;
-  return 0;
+uint8_t MAX116xx::writeSetup(uint8_t reference, uint8_t clockSource, uint8_t bipUni, uint8_t resetConfig) {
+  uint8_t setupByte = 0x80 // Start with MSB=1 for setup byte
+    | (reference & 0x07) << 4     //Reference select are bits 6:4
+    | (clockSource & 0x01) << 3   //clock source bit is bit 3
+    | (bipUni &0x01) << 2         //bipolar/unipolar set is bit 2
+    | (resetConfig & 0x01)<<1;    //reset or keep config is bit 1
+    //Bit 0 is unused in setup
+
+  Wire.beginTransmission(mAddress);
+  Wire.write(setupByte);
+  return Wire.endTransmission();
+
 }
 
-void MAX116xx::writeThresholdHigh(uint16_t threshold) {
-  writeRegister(MAX116xx_THRESHOLD_HIGH, threshold >> 8);
-  writeRegister(MAX116xx_THRESHOLD_LOW, threshold & 0xff);
+uint8_t MAX116xx::writeConfig(uint8_t scanMode, uint8_t channel, uint8_t singleDif) {
+  uint8_t configByte = 0x00 // MSB 0 for configByte
+    | (scanMode & 0x03) << 5 //Scanmode bits are bits 5 & 6
+    | (channel & 0x0F) << 1  //channel bits are bits 4:1
+    | (singleDif & 0x01);     //Single/Differential bit is bit 0
+
+  Wire.beginTransmission(mAddress);
+  Wire.write(configByte);
+  return Wire.endTransmission();
 }
+
+
+
+// uint8_t MAX116xx::readConfig() {
+//   uint8_t data;
+//   uint8_t err = readRegister(MAX116xx_CONFIG, &data);
+//   if(err==0) return data;
+//   return 0;
+// }
+
+// void MAX116xx::writeThresholdHigh(uint16_t threshold) {
+//   writeRegister(MAX116xx_THRESHOLD_HIGH, threshold >> 8);
+//   writeRegister(MAX116xx_THRESHOLD_LOW, threshold & 0xff);
+// }
--- src/MAX116xx.h
+++ src/MAX116xx.h
@@ -11,29 +11,44 @@
 #define MAX11610_ADDRESS 0x35
 #define MAX11611_ADDRESS 0x35
 
-// Register addresses
-#define MAX116xx_CONVERSION 0x00
-#define MAX116xx_CONFIG 0x01
-#define MAX116xx_THRESHOLD_HIGH 0x02
-#define MAX116xx_THRESHOLD_LOW 0x03
-#define MAX116xx_STATUS 0x04
-
 class MAX116xx {
 public:
   MAX116xx(int address, int scl = SCL, int sda = SDA);
   // MAX116xx(int address, TwoWire *twoWire);
   void begin();
   uint16_t readADC(uint8_t channel);
+
+  uint8_t writeSetup(uint8_t setup);
+  uint8_t writeSetup(uint8_t reference, uint8_t clockSource, uint8_t bipUni, uint8_t resetConfig);
   uint8_t writeConfig(uint8_t config);
-  uint8_t readConfig();
-  void writeThresholdHigh(uint16_t threshold);
-  uint16_t readThresholdHigh();
-  void writeThresholdLow(uint16_t threshold);
-  uint16_t readThresholdLow();
-  uint8_t readStatus();
+  uint8_t writeConfig(uint8_t scanMode, uint8_t channel, uint8_t adcMode);
+
+  static const uint8_t REFERENCE_VDD            = 0x00; //Use VDD as reference voltage (5V/3V3)
+  static const uint8_t REFERENCE_EXTERNAL       = 0x02; //Use external reference on pin (AINX/)REF
+  static const uint8_t REFERENCE_INTERNAL       = 0x05; //Use internal reference (4.096V/2.048V) with external (AINX/)REF pin as ADC in
+  static const uint8_t REFERENCE_INTERNAL_OUT   = 0x07; //use internal reference (4.096V/2.048V) and output reference on pin (AINX/)REF
+
+  static const uint8_t CLOCK_INTERNAL           = 0x00; //Use the internal clock chip for conversion
+  static const uint8_t CLOCK_EXTERNAL           = 0x01; //Use SCL as clock source
+
+  static const uint8_t SETUP_KEEP_CONFIG        = 0x00; //Make no changes to the config byte during setup write
+  static const uint8_t SETUP_RESET_CONFIG       = 0x01; //Rest config byte during setup-write
+
+  static const uint8_t MODE_UNIPOLAR            = 0x00; //Run differential ADC in unipolar mode (only positive voltages from 0 to REF)
+  static const uint8_t MODE_BIPOLAR             = 0x01; //Run differential ADC in bipolar mode (swing positive/negative around 1/2Vref), ignored in single-ended mode
+
+  static const uint8_t SCANMODE_UPTO            = 0x00; //scans from AIN0 up to selected channel
+  static const uint8_t SCANMODE_REPEATED        = 0x01; //converts selected channel 8 times
+  static const uint8_t SCANMODE_UPTO_UPPERHALF  = 0x02; //scans up from AIN6(11608/09/10/11) or up from AIN2 (11606/07) to selected channel
+  static const uint8_t SCANMODE_SINGLE          = 0x03; //converts selected channel once
+
+  static const uint8_t ADCMODE_SINGLEENDED     = 0x01; //Configure ADC to run in single-ended mode
+  static const uint8_t ADCMODE_DIFFERENTIAL    = 0x00; //Configure ADC to run in differential mode
 
 private:
   int mAddress;
+  uint8_t mAdcCount;
+  uint8_t mRefVoltage;
   uint8_t readRegister(uint8_t reg, uint8_t *data);
   uint8_t readRegister(uint8_t reg, uint8_t *data, uint8_t len);
   uint8_t writeRegister(uint8_t reg, uint8_t value);
